3411498b46d74d73834526e3485ce6e0bf0e4e3e
boikanyothebe <boikanyo.thebe@gmail.com>
2025-10-17 14:50:01 +0200

    NEW INDEX

--- script.js
+++
@@ -1,6 +0,0 @@
-// Basic JS for Lost and Found Web UI
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Placeholder for future scripts
-    console.log('Lost and Found Web UI loaded.');
-});
